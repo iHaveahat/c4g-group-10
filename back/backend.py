3da53f295d1c1c6110752ff86739914c0853319a
Ravageer <97468199+Ravageer@users.noreply.github.com>
2025-07-18 13:43:07 -0400

    Update backend.py

--- back/backend.py
+++ back/backend.py
@@ -1,7 +1,7 @@
 from back.deberta_loader import predict_deberta, model
 from predictor import predict_text
 from back.new_deberta_loader import predict, model
-modelUse = "tfif"
+modelUse = "tff"
 def predict_fake_news_with_confidence(text: str) -> dict:
     """
     Routes prediction to the model.
@@ -9,7 +9,7 @@ def predict_fake_news_with_confidence(text: str) -> dict:
     if modelUse == "tfidf":
         return predict_text(text)
     # return {"result": "❌ Empty text", "confidence": 0.0}
-    else if modelUse=="tfif":
+    elif modelUse=="tfif":
          return predict_deberta(text)
-    else
+    else:
         return predict(text)
